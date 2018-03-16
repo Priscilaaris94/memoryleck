7d6cbbdad40a46af32374bdfab1f2abe5d784902
krosenk729 <krosenk729@gmail.com>
2018-03-16 14:35:13 -0400

    user model updated

---
+++ src/models/users.js
@@ -0,0 +1,15 @@
+module.exports = function(sequelize, DataTypes) {
+  var Users = sequelize.define("Users", {
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    type: DataTypes.STRING, // either 'tenet' or 'landlord'
+  });
+
+  Users.associate = function(models) {
+    Users.hasMany(models.Property, {
+      onDelete: "cascade"
+    });
+  };
+
+  return Users;
+};
