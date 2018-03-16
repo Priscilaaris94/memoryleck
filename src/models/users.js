7c995fb24d6dc20993da3b4ff73c4965ced69d49
krosenk729 <krosenk729@gmail.com>
2018-03-16 14:42:53 -0400

    mockup changes

--- src/models/users.js
+++ src/models/users.js
@@ -2,7 +2,7 @@ module.exports = function(sequelize, DataTypes) {
   var Users = sequelize.define("Users", {
     name: DataTypes.STRING,
     email: DataTypes.STRING,
-    type: DataTypes.STRING, // either 'tenet' or 'landlord'
+    type: DataTypes.STRING // either 'tenet' or 'landlord'
   });
 
   Users.associate = function(models) {
