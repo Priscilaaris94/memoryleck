eb7a7aaec89c529012075c0febdfb371130a5516
krosenk729 <krosenk729@gmail.com>
2018-03-20 20:46:08 -0400

    ejs is serving ok

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-//Dependencies
+// Dependencies
 
 var express = require("express");
-
 var bodyParser = require("body-parser");
 
 
 
-//Define port the server will be listening on.
+// Define port the server will be listening on.
 
 var PORT = process.env.PORT || 3000;
 
@@ -16,33 +15,56 @@ var app = express();
 
 
 
-//Serve static content for the app from the "public" directory in the application directory.
-
+// Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(__dirname + '/app/public'));
 
-
-
-//Parse application/x-www-form-urlencoded
-
+// Parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
-
-
-
-//Parse application/json
-
 app.use(bodyParser.json());
 
+// Views 
+var path = require('path');
+app.set('views', path.join(__dirname, 'app', 'views'));
+app.set('view engine', 'ejs');
 
+app.get('/', function(req, res){ 
+	properties = [{
+		address_one: "118 Peachtree Street",
+		address_two: null,
+		baths: 1,
+		beds: 2,
+		city: "Atlanta",
+		id: 2,
+		img_1: "https://lorempixel.com/g/400/400/cats",
+		landlord_id: "bC8Ol7BDdoY6AZD10w2vRmU0Pab2",
+		price: 1200,
+		sqfeet: 860,
+		state: "GA",
+		status: "vacant",
+		tenant_id: null,
+		zip: 30302
+	}, 
+	{
+		address_one: "567 Peachtree Road",
+		address_two: null,
+		baths: 1,
+		beds: 2,
+		city: "Atlanta",
+		id: 2,
+		img_1: "https://lorempixel.com/g/400/400/cats",
+		landlord_id: "bC8Ol7BDdoY6AZD10w2vRmU0Pab2",
+		price: 1150,
+		sqfeet: 920,
+		state: "GA",
+		status: "vacant",
+		tenant_id: null,
+		zip: 30302
+	}];
+	// orm.getproperties( ... callback() );
+	// fetch('/api/property').then(b => b.json()).then( d => console.log(d) )
+ 	res.render('pages/allproperties',{title: 'All Properties',properties: properties});
+});
 
-//Set Handlebars.
-
-var exphbs = require("express-handlebars");
-
-
-
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-
-app.set("view engine", "handlebars");
 
 
 
