2ee1638aabbc34a24ba002da390256235ac74df2
krosenk729 <krosenk729@gmail.com>
2018-03-23 15:05:43 -0400

    adding momentjs

--- app/public/scripts/dashboard.js
+++ app/public/scripts/dashboard.js
@@ -4,7 +4,9 @@
 		// Clean Dates
 		let dateCells = $('td.payment-date');
 		for(let i = 0, x = dateCells.length; i < x; i++){
-			$(dateCells[i]).text( $(dateCells[i]).text().replace(' 00:00:00 GMT-0500 (Eastern Standard Time)','') );
+			let newtext = $(dateCells[i]).text().trim().slice(0, 16);
+			newtext += '<span class="moment-count">' + moment(newtext).fromNow() + '</span>';
+			$(dateCells[i]).html( newtext );
 		}
 	});
 })();
